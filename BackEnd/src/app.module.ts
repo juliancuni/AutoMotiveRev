de63cd03c04e531e61575d700413f6b18b298072
Julian Cuni <julian.cuni@microservices.al>
2021-08-23 13:04:16 +0200

    TypeOrm & Postgress

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(<string>process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
