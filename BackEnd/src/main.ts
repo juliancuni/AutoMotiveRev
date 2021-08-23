03ac3d9d637feb49a88e94d1bb20629b4b9c15e1
Julian Cuni <julian.cuni@microservices.al>
2021-08-23 21:04:38 +0200

    Helmet, CORS, SERVER_PORT

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,26 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
-
+  app.use(helmet());
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
-    const options: SwaggerDocumentOptions =  {
-      operationIdFactory: (
-        controllerKey: string,
-        methodKey: string
-      ) => methodKey
-    };
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('openapi', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
